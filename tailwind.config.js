40bfe67d9292d4ed74fc329fd3920d74fe6fd40f
chiamaka28 <chiamakamaduike@gmail.com>
2024-04-24 10:01:41 +0100

    added styles to the background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: { 
+      colors: {
+      'nutmeg': 'hsl(14, 45%, 36%)',
+      'dark-raspberry': 'hsl(332, 51%, 32%)',
+      'rose-white': 'hsl(330, 100%, 98%)',
+      'eggshell': 'hsl(30, 54%, 90%)',
+      'light-grey': ' hsl(30, 18%, 87%)',
+      'wenge-brown': 'hsl(30, 10%, 34%)',
+      'dark-charcoal':' hsl(24, 5%, 18%)',
+    },
+  },
   },
   plugins: [],
 }
